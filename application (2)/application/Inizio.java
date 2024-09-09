15448f3ad21078ee3c75040c84481e82d2b0b2ac
jeffAlan04 <jeffalan.shuyinta@studio.unibo.it>
2024-09-09 19:31:35 +0200

    Grafica

--- application (2)/application/Inizio.java
+++ application (2)/application/Inizio.java
@@ -3,6 +3,7 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Inizio {
